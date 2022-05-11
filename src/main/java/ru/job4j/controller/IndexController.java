aa5d77521acb9c874dabe4bad743e3cde1f7cdc9
Sergei159 <qars2010@yandex.ru>
2022-05-12 00:30:32 +0500

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/controller/IndexController.java
+++ src/main/java/ru/job4j/controller/IndexController.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexController {
 
     @GetMapping("/index")
     public String index(Model model) {
+        List<Integer> integerList = List.of(1, 5, 6, 7, 8);
+        model.addAttribute("integerList", integerList);
         return "index";
     }
 }
